717c4aaf0125d8aadabc0c62585377cd6bf50647
Fullbuster <Fullbuster0033@gmail.com>
2017-11-01 10:32:47 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        new Main();
+
+    }
+    Main(){
+        int n;
+        double num1=0,num2=1,num;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Введіть n");
+        n=scan.nextInt();
+        for (int i=0;i<n;i++){
+            num=num1+num2;
+            num1=num2;
+            num2=num;
+            int numint=(int) num;
+            System.out.println(""+numint);
+        }
+    }
+}
